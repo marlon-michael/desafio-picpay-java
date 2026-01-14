d3353b4ea1d832c19b119addc1c2009565be880b
Marlon Michael <marlonmich7@gmail.com>
2026-01-14 17:43:11 -0300

    adicionadas validacoes simples dos campos de Account - adicionado construtor de Account

--- pixpay/src/main/java/com/desafio/pixpay/core/domain/Account.java
+++ pixpay/src/main/java/com/desafio/pixpay/core/domain/Account.java
@@ -11,6 +11,44 @@ public class Account {
     String password;
     Long balanceInPipsOfReal;
 
+    public Account(UUID id, String identificationNumber, String fullName, String email, String password, Long balanceInPipsOfReal) {
+        setId(id);
+        setIdentificationNumber(identificationNumber);
+        setFullName(fullName);
+        setEmail(email);
+        setPassword(password);
+        setBalanceInPipsOfReal(balanceInPipsOfReal);
+    }
+
+    public boolean isEmailValid(String email) {
+        return false;
+    }
+
+    public boolean isPasswordValid(String password) {
+        //String validPasswordRegexWithNoInvalidChars = "^(?!.*[\\s\\\\;\"'<>/|\\*\\(\\)\\[\\]{}]).*(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$.&%?_+]).+$";
+        // password must contain one uppercase letter, one lowercase letter, one number and at least one special character: , . ! @ # $ & % ? _ +
+        String validPasswordRegex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$.,&%?_+]).+$";
+        boolean isValid = password.matches(validPasswordRegex);
+        if (!isValid){
+            throw new IllegalArgumentException("The password must contain one uppercase letter, one lowercase letter, one number and at least one of this special character: , . ! @ # $ & % ? _ +");
+        }
+        return isValid;
+    }
+
+    public boolean isStringValid(String string) {
+        // Regex detalhado:
+        // \\\\  -> Corresponde a uma contrabarra literal (\)
+        // \\s    -> Corresponde espaços, tabulações e quebras de linha
+        // \"    -> Corresponde a aspas duplas (")
+        // /, ;, ', <, >, |, - , * , ( , ) , [ , ] , { , } -> Corresponde aos respectivos caracteres literais
+        String invalidRegex = ".*[\\s\\\\;\"'<>/|\\-\\-\\*\\(\\)\\[\\]{}].*";
+        boolean isValid = !string.matches(invalidRegex);
+        if (!isValid) {
+            throw new IllegalArgumentException("The fields cannot contain this special character: \\ / | * ( ) [ ] { } ; ' \" < > or spaces");
+        }
+        return isValid;
+    }
+
     // Getters and Setters
     public UUID getId() {
         return id;
@@ -25,6 +63,10 @@ public class Account {
     }
 
     public void setIdentificationNumber(String identificationNumber) {
+        isStringValid(identificationNumber);
+        if (identificationNumber.length() < 9 || identificationNumber.length() > 14) {
+            throw new IllegalArgumentException("The identification number must be between 9 and 14 characters long. Ex: 000.000.000-00 or 00.000.000/0000-00");
+        }
         this.identificationNumber = identificationNumber;
     }
 
@@ -33,6 +75,10 @@ public class Account {
     }
 
     public void setFullName(String fullName) {
+        isStringValid(fullName);
+        if (fullName.length() < 1 || fullName.length() > 40) {
+            throw new IllegalArgumentException("The full name must be between 1 and 40 characters long.");
+        }
         this.fullName = fullName;
     }
 
@@ -41,6 +87,9 @@ public class Account {
     }
 
     public void setEmail(String email) {
+        if (email.length() < 3 || email.length() > 254) {
+            throw new IllegalArgumentException("The email must be between 3 and 254 characters long.");
+        }
         this.email = email;
     }
 
@@ -49,6 +98,11 @@ public class Account {
     }
 
     public void setPassword(String password) {
+        isStringValid(password);
+        isPasswordValid(password);
+        if (password.length() < 8 || password.length() > 18) {
+            throw new IllegalArgumentException("The password must be between 8 and 18 characters long.");
+        }
         this.password = password;
     }
 
