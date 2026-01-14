b11fa17920d03f3587de730181d2dd0b17bbc604
Marlon Michael <marlonmich7@gmail.com>
2026-01-14 10:49:48 -0300

    configuracao do projeto pixpay

---
+++ pixpay/src/main/java/com/desafio/pixpay/PixpayApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.pixpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixpayApplication.class, args);
+	}
+
+}
---
+++ pixpay/src/test/java/com/desafio/pixpay/PixpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.pixpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
