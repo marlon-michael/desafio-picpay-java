7c6eeca9026ec6c280f41c0dd49cf8050c4a41e0
Marlon Michael <marlonmich7@gmail.com>
2026-01-14 11:08:02 -0300

    removido dependencia lombok - adicionado getters e setters

--- pixpay/src/main/java/com/desafio/pixpay/core/domain/User.java
+++ pixpay/src/main/java/com/desafio/pixpay/core/domain/User.java
@@ -1,9 +1,8 @@
 package com.desafio.pixpay.core.domain;
 
 import java.util.UUID;
-import lombok.Data;
 
-@Data
+
 public class User {
     UUID id;
     String identificacao;
@@ -11,4 +10,53 @@ public class User {
     String email;
     String password;
     Long saldo;
+
+    // Getters and Setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getIdentificacao() {
+        return identificacao;
+    }
+
+    public void setIdentificacao(String identificacao) {
+        this.identificacao = identificacao;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Long saldo) {
+        this.saldo = saldo;
+    }
 }
