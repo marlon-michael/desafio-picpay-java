242aa552f8eab4150881c0cf073d44e3f63447c0
Marlon Michael <marlonmich7@gmail.com>
2026-01-15 11:19:30 -0300

    adicionado contrutor com validacao de email

--- pixpay/src/main/java/com/desafio/pixpay/core/usecases/CreateAccountUseCase.java
+++ pixpay/src/main/java/com/desafio/pixpay/core/usecases/CreateAccountUseCase.java
@@ -3,15 +3,27 @@ package com.desafio.pixpay.core.usecases;
 
 import com.desafio.pixpay.core.domain.Account;
 import com.desafio.pixpay.core.gateways.AccountGateway;
+import com.desafio.pixpay.core.gateways.EmailValidatorGateway;
 
 public class CreateAccountUseCase {
-    private AccountGateway accountGateway;
+    private final AccountGateway accountGateway;
+    private final EmailValidatorGateway emailValidator;
 
-    public CreateAccountUseCase(AccountGateway accountGateway) {
+    public CreateAccountUseCase(AccountGateway accountGateway, EmailValidatorGateway emailValidator) {
         this.accountGateway = accountGateway;
+        this.emailValidator = emailValidator;
     }
 
-    public Account execute(Account account){
+    public Account execute(Account accountRequest) {
+        Account account = new Account(
+            accountRequest.getId(),
+            accountRequest.getIdentificationNumber(),
+            accountRequest.getFullName(),
+            accountRequest.getEmail(),
+            accountRequest.getPassword(),
+            accountRequest.getBalanceInPipsOfReal(),
+            emailValidator
+        );
         accountGateway.saveAccount(account);
         return account;
     }
