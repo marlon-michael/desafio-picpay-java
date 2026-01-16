afddf298555b914a5ebea83637e41e05086d751d
Marlon Michael <marlonmich7@gmail.com>
2026-01-16 18:01:01 -0300

    mapeador de Account para AccountEntity

---
+++ pixpay/src/main/java/com/desafio/pixpay/infra/persistence/AccountMapper.java
@@ -0,0 +1,45 @@
+package com.desafio.pixpay.infra.persistence;
+
+import com.desafio.pixpay.core.domain.account.Account;
+import com.desafio.pixpay.core.domain.account.AccountTypeEnum;
+import com.desafio.pixpay.core.domain.identification.IdentificationTypeEnum;
+import com.desafio.pixpay.core.service.AccountValidatorService;
+
+public class AccountMapper {
+    private static AccountValidatorService accountValidatorService;
+
+    public AccountMapper(AccountValidatorService accountValidatorService) {
+        AccountMapper.accountValidatorService = accountValidatorService;
+    }
+
+    public AccountEntity fromDomainToEntity(Account account) {
+        if (!accountValidatorService.isAccountValid(account)){
+            throw  new IllegalArgumentException("Invalid account");
+        }
+        AccountEntity accountEntity = new AccountEntity(
+            account.getId(),
+            account.getAccountType().getValue(),
+            account.getIdentificationType().getValue(),
+            account.getIdentificationNumber(),
+            account.getFullName(),
+            account.getEmail(),
+            account.getPassword(),
+            account.getBalanceInPipsOfReal()
+        );
+        return accountEntity;
+    }
+
+    public Account fromEntityToDomain(AccountEntity accountEntity) {
+        Account account = new Account(
+            accountEntity.getId(),
+            AccountTypeEnum.fromValue(accountEntity.getAccountType()),
+            IdentificationTypeEnum.fromValue(accountEntity.getIdentificationType()),
+            accountEntity.getIdentificationNumber(),
+            accountEntity.getFullName(),
+            accountEntity.getEmail(),
+            accountEntity.getPassword(),
+            accountEntity.getBalanceInPipsOfReal()
+        );
+        return account;
+    }
+}
