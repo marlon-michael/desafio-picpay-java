2a95f3adf20ee5d1617427c3e4ab3b0c191375e1
Marlon Michael <marlonmich7@gmail.com>
2026-01-16 18:14:42 -0300

    alterado para novo mapeamento de entidades

--- pixpay/src/main/java/com/desafio/pixpay/infra/persistence/AccountRepository.java
+++ pixpay/src/main/java/com/desafio/pixpay/infra/persistence/AccountRepository.java
@@ -2,20 +2,22 @@ package com.desafio.pixpay.infra.persistence;
 
 import org.springframework.stereotype.Repository;
 
-import com.desafio.pixpay.core.domain.Account;
+import com.desafio.pixpay.core.domain.account.Account;
 import com.desafio.pixpay.core.gateways.AccountGateway;
 
 @Repository
 public class AccountRepository implements AccountGateway {
     private final JpaAccountRepository jpaAccountRepository;
+    private final AccountMapper accountMapper;
 
-    public AccountRepository(JpaAccountRepository jpaAccountRepository) {
+    public AccountRepository(JpaAccountRepository jpaAccountRepository, AccountMapper accountMapper) {
         this.jpaAccountRepository = jpaAccountRepository;
+        this.accountMapper = accountMapper;
     }
 
     @Override
     public void saveAccount(Account account) {
-        jpaAccountRepository.save(AccountEntity.fromDomainToEntity(account));
+        jpaAccountRepository.save(accountMapper.fromDomainToEntity(account));
     }
     
 }
