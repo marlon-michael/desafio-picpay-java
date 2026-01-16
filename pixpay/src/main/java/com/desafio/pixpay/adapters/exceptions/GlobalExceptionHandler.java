13d651c4ea3ad353ea0c2d3a50527b37039a2b31
Marlon Michael <marlonmich7@gmail.com>
2026-01-16 18:40:50 -0300

    excecoes IllegalArgument sao mapeadas para ResponseEntity

---
+++ pixpay/src/main/java/com/desafio/pixpay/adapters/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.desafio.pixpay.adapters.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler; // Esta é a anotação
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+}
