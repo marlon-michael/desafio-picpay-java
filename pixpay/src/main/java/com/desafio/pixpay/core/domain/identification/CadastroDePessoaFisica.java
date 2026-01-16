aed125333a71902819bfde8a86b61d37ce215f80
Marlon Michael <marlonmich7@gmail.com>
2026-01-16 18:41:30 -0300

    corrigidos erros de typo

--- pixpay/src/main/java/com/desafio/pixpay/core/domain/identification/CadastroDePessoaFisica.java
+++ pixpay/src/main/java/com/desafio/pixpay/core/domain/identification/CadastroDePessoaFisica.java
@@ -5,7 +5,7 @@ public class CadastroDePessoaFisica extends IdentificationNumber {
     public CadastroDePessoaFisica(String identificationCode) {
         super();
         if (identificationCode.length() != 11){
-            throw new IllegalArgumentException("The identification number for Cadastro De Pessoa Fisica must be 11 characters long. Ex: 000.000.000-00");
+            throw new IllegalArgumentException("The identification number for Cadastro De Pessoa Fisica must be 11 characters long. Ex: 000.000.000-00.");
         }
         if (!identificationCode.matches("^[0-9]{11}$")) {
             throw new IllegalArgumentException("The identification number for Cadastro De Pessoa Fisica must contain only numbers.");
