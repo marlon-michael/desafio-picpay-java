5e513462c5a04efb129e2a4c556acba456666afe
Marlon Michael <marlonmich7@gmail.com>
2026-01-16 18:13:26 -0300

    configuracao de beans

--- pixpay/src/main/java/com/desafio/pixpay/infra/configuration/AccountConfig.java
+++ pixpay/src/main/java/com/desafio/pixpay/infra/configuration/AccountConfig.java
@@ -5,20 +5,31 @@ import org.springframework.context.annotation.Configuration;
 
 import com.desafio.pixpay.core.gateways.AccountGateway;
 import com.desafio.pixpay.core.gateways.EmailValidatorGateway;
-import com.desafio.pixpay.core.gateways.IdentificationValidator;
+import com.desafio.pixpay.core.gateways.IdentificationValidatorGateway;
+import com.desafio.pixpay.core.service.AccountValidatorService;
 import com.desafio.pixpay.core.usecases.CreateAccountUseCase;
 import com.desafio.pixpay.infra.persistence.JpaAccountRepository;
 import com.desafio.pixpay.infra.validation.JMailValidator;
 import com.desafio.pixpay.infra.validation.JakartaIdentificationValidator;
+import com.desafio.pixpay.infra.persistence.AccountMapper;
 import com.desafio.pixpay.infra.persistence.AccountRepository;
 
 @Configuration
 public class AccountConfig {
 
     @Bean
-    public AccountGateway accountGateway(JpaAccountRepository jpaAccountRepository){
-        return new AccountRepository(jpaAccountRepository);
+    public AccountMapper accountMapper(AccountValidatorService accountValidatorService){
+        return new AccountMapper(accountValidatorService);
     }
+
+    @Bean
+    public AccountGateway accountGateway(JpaAccountRepository jpaAccountRepository, AccountMapper accountMapper){
+        return new AccountRepository(jpaAccountRepository, accountMapper);
+    }
+
+    @Bean AccountValidatorService accountValidatorService(EmailValidatorGateway emailValidatorGateway, IdentificationValidatorGateway identificationValidatorGateway){
+        return new AccountValidatorService(identificationValidatorGateway, emailValidatorGateway);
+    }   
 
     @Bean
     public EmailValidatorGateway emailValidatorGateway(){
@@ -26,12 +37,12 @@ public class AccountConfig {
     }
 
     @Bean
-    public IdentificationValidator identificationValidator(){
+    public IdentificationValidatorGateway identificationValidator(){
         return new JakartaIdentificationValidator();
     }
 
     @Bean
-    public CreateAccountUseCase createAccountUseCase(AccountGateway accountGateway, EmailValidatorGateway emailValidatorGateway){
-        return new CreateAccountUseCase(accountGateway, emailValidatorGateway);
+    public CreateAccountUseCase createAccountUseCase(AccountGateway  accountGateway, AccountValidatorService accountValidatorService){
+        return new CreateAccountUseCase(accountGateway, accountValidatorService);
     }
 }
