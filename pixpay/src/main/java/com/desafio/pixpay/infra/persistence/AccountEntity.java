5c7240c4844252d736e10941e3877a2d2f3e781d
Marlon Michael <marlonmich7@gmail.com>
2026-01-16 18:01:51 -0300

    extraido mapeamento de entidades para nova classe

--- pixpay/src/main/java/com/desafio/pixpay/infra/persistence/AccountEntity.java
+++ pixpay/src/main/java/com/desafio/pixpay/infra/persistence/AccountEntity.java
@@ -2,13 +2,7 @@ package com.desafio.pixpay.infra.persistence;
 
 import java.util.UUID;
 
-import com.desafio.pixpay.core.domain.Account;
-import com.desafio.pixpay.core.domain.AccountTypeEnum;
-import com.desafio.pixpay.core.domain.identification.IdentificationTypeEnum;
-
 import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -18,7 +12,6 @@ import lombok.Data;
 @Entity
 public class AccountEntity {
     @Id()
-    //@GeneratedValue(strategy = GenerationType.UUID)
     UUID id;
     String accountType;
     String identificationType;
@@ -39,33 +32,5 @@ public class AccountEntity {
         this.email = email;
         this.password = password;
         this.balanceInPipsOfReal = balanceInPipsOfReal;
-    }
-
-    public static AccountEntity fromDomainToEntity(Account account) {
-        AccountEntity accountEntity = new AccountEntity(
-            account.getId(),
-            account.getAccountType().getValue(),
-            account.getIdentificationType().getValue(),
-            account.getIdentificationNumber(),
-            account.getFullName(),
-            account.getEmail(),
-            account.getPassword(),
-            account.getBalanceInPipsOfReal()
-        );
-        return accountEntity;
-    }
-
-    public static Account fromEntityTo1Domain(AccountEntity accountEntity) {
-        Account account = new Account(
-            accountEntity.getId(),
-            AccountTypeEnum.fromValue(accountEntity.getAccountType()),
-            IdentificationTypeEnum.fromValue(accountEntity.getIdentificationType()),
-            accountEntity.getIdentificationNumber(),
-            accountEntity.getFullName(),
-            accountEntity.getEmail(),
-            accountEntity.getPassword(),
-            accountEntity.getBalanceInPipsOfReal()
-        );
-        return account;
     }
 }
