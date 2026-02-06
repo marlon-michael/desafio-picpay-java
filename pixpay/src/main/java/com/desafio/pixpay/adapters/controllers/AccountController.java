df040325f2658e5f2808f379687365da81d6cceb
Marlon Michael <marlonmich7@gmail.com>
2026-02-06 16:21:49 -0300

    .

--- pixpay/src/main/java/com/desafio/pixpay/adapters/controllers/AccountController.java
+++ pixpay/src/main/java/com/desafio/pixpay/adapters/controllers/AccountController.java
@@ -1,28 +1,28 @@
 package com.desafio.pixpay.adapters.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.desafio.pixpay.adapters.dtos.SaveAccountDTO;
-import com.desafio.pixpay.core.domain.account.Account;
-import com.desafio.pixpay.core.usecases.CreateAccountUseCase;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import com.desafio.pixpay.infra.persistence.entity.AccountEntity;
+import com.desafio.pixpay.infra.persistence.repository.AccountRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
-@RequestMapping("/accounts")
+@RequestMapping("accounts")
 public class AccountController {
-    
-    @Autowired
-    private CreateAccountUseCase createAccountUseCase;
 
-    
-    @PostMapping
-    public ResponseEntity<String> createAccount(@RequestBody SaveAccountDTO saveAccountDTO) {
-        Account createdAccount = createAccountUseCase.execute(saveAccountDTO);
-        return ResponseEntity.status(201).body("Account " + createdAccount.getFullName() + " created successfully");
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Deprecated 
+    @GetMapping()
+    public List<AccountEntity> getAccounts() {
+        return accountRepository.findAll();
     }
+    
 }
