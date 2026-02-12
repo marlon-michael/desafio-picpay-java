7ac0d43ea7d787ae1d6c0999dbe8e03195c73acb
Marlon Michael <marlonmich7@gmail.com>
2026-02-12 16:59:38 -0300

    mapeado valor de retorno id

--- pixpay/src/main/java/com/desafio/pixpay/adapters/dtos/ListAccountByManagerDTO.java
+++ pixpay/src/main/java/com/desafio/pixpay/adapters/dtos/ListAccountByManagerDTO.java
@@ -1,10 +1,12 @@
 package com.desafio.pixpay.adapters.dtos;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.desafio.pixpay.core.domain.account.Account;
 
 public record ListAccountByManagerDTO(
+        UUID id,
         List<String> roles,
         String accountType,
         String identificationType,
@@ -16,6 +18,7 @@ public record ListAccountByManagerDTO(
         String lastModifiedAt) {
     public static ListAccountByManagerDTO fromAccount(Account account) {
         return new ListAccountByManagerDTO(
+                account.getId(),
                 account.getRoles().stream().map(role -> role.getValue()).toList(),
                 account.getAccountType().getValue(),
                 account.getIdentificationType().getValue(),
