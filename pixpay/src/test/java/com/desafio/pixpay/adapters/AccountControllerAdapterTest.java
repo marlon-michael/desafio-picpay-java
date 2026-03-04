ee27104a2c3a7725b198034b753ba65952528837
Marlon Michael <marlonmich7@gmail.com>
2026-03-04 16:48:53 -0300

    testes de integracao

--- pixpay/src/test/java/com/desafio/pixpay/adapters/AccountControllerAdapterTest.java
+++ pixpay/src/test/java/com/desafio/pixpay/adapters/AccountControllerAdapterTest.java
@@ -1,5 +1,6 @@
 package com.desafio.pixpay.adapters;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -7,60 +8,131 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-// import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
-
 import com.desafio.pixpay.adapters.dtos.SaveAccountDTO;
+import com.desafio.pixpay.core.domain.account.Account;
+import com.desafio.pixpay.infra.persistence.repository.AccountRepository;
+import com.desafio.pixpay.utils.AccountFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.Cookie;
 
 
 @SpringBootTest
-@ActiveProfiles("test") // Usa o application-test.properties
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 @Transactional
-// @Rollback(false)
+@Rollback(false)
 public class AccountControllerAdapterTest {
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
+    private AccountRepository accountRepository;
 
+    @Autowired
     private MockMvc mockMvc;
+
     private ObjectMapper objectMapper;
+    private Cookie managerCookie;
+    private Cookie businessCokie;
+    private Cookie personalCookie;
+    private Account managerAccount;
+    private Account businessAccount;
+    private Account personalAccount;
+
+    Cookie performLogin(String username, String password) throws Exception {
+        MvcResult result = mockMvc.perform(
+            post("/authenticate")
+            .with(httpBasic(username, password))
+        )
+        .andExpect(status().isOk())
+        .andReturn();
+        System.out.println("COOKIE: "+result.getResponse().getCookie("jwt-token"));
+        return result.getResponse().getCookie("jwt-token");
+    }
 
     @BeforeEach
-    void setUp() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    void setUp() throws Exception {
         objectMapper = new ObjectMapper();
+
+        setupCreateAccounts();
+        setupLoginAccounts();
+    }
+
+    void setupCreateAccounts() {
+        managerAccount = AccountFactory.createManager();
+        businessAccount = AccountFactory.createBusiness();
+        personalAccount = AccountFactory.createPersonal();
+        
+        if (accountRepository.findByIdentificationNumber(managerAccount.getIdentification().getIdentificationNumber()) == null){
+            accountRepository.create(managerAccount);
+            System.out.println("salvo 1");
+        }
+        if (accountRepository.findByIdentificationNumber(businessAccount.getIdentification().getIdentificationNumber()) == null){
+            accountRepository.create(businessAccount);
+            System.out.println("salvo 2");
+        }
+        if (accountRepository.findByIdentificationNumber(personalAccount.getIdentification().getIdentificationNumber()) == null){
+            accountRepository.create(personalAccount);
+            System.out.println("salvo 3");
+        }
+        System.out.println("accs="+accountRepository.findByIdentificationNumber(managerAccount.getIdentification().getIdentificationNumber()).getEmail().getValue());
+    }
+
+    void setupLoginAccounts() throws Exception{
+        System.out.println("login 1");
+        managerCookie = performLogin(managerAccount.getIdentification().getIdentificationNumber(), managerAccount.getIdentification().getIdentificationNumber());
+        System.out.println("login 2");
+        businessCokie = performLogin(businessAccount.getIdentification().getIdentificationNumber(), businessAccount.getIdentification().getIdentificationNumber());
+        System.out.println("login 3");
+        personalCookie = performLogin(personalAccount.getIdentification().getIdentificationNumber(), personalAccount.getIdentification().getIdentificationNumber());
+        System.out.println("cookie="+managerCookie.getValue());
+    }
+
+    @Test
+    void shouldListAccounts() throws Exception{
+        System.out.println("shouldListAccounts()");
+        mockMvc.perform(
+            get("/accounts")
+            .cookie(managerCookie)
+        )
+        .andExpect(jsonPath("$").isArray())
+        .andExpect(jsonPath("$[0].id").exists())
+        .andExpect(jsonPath("$[0].fullName").exists())
+        .andExpect(jsonPath("$[0].email").exists())
+        .andExpect(jsonPath("$[0].roles").exists())
+        .andExpect(status().isOk());
     }
 
     @Test
     void shouldCreateAccount() throws Exception {
         SaveAccountDTO saveAccountDTO = new SaveAccountDTO(
-            "PERSONAL",
             "CadastroDePessoaFisica",
-            "07007826997",
+            "54104171000",
             "Marlon Michael",
-            "marlonmich7@gmail.com",
-            "Qypu4013?"
+            "marlon@gmail.com",
+            "Marlon@Michael1"
         );
 
         String accountJson = objectMapper.writeValueAsString(saveAccountDTO);
 
-        mockMvc.perform(post("/accounts")
+        mockMvc.perform(
+            post("/signup")
             .contentType(MediaType.APPLICATION_JSON)
-            .content(accountJson))
-            .andExpect(status().isCreated())
-            .andExpect(content().string("Account Marlon Michael created successfully"));
+            .content(accountJson)
+        )
+        .andExpect(status().isCreated())
+        .andExpect(content().string("Account Marlon Michael created successfully."));
     }
 
     @Test
     void shouldThrowCPFInvalidException() throws Exception {
         SaveAccountDTO saveAccountDTO = new SaveAccountDTO(
-            "PERSONAL",
             "CadastroDePessoaFisica",
             "12345678910",
             "John Doe",
@@ -70,7 +142,7 @@ public class AccountControllerAdapterTest {
 
         String accountJson = objectMapper.writeValueAsString(saveAccountDTO);
 
-        mockMvc.perform(post("/accounts")
+        mockMvc.perform(post("/signup")
             .contentType(MediaType.APPLICATION_JSON)
             .content(accountJson))
             .andExpect(status().isBadRequest())
---
+++ pixpay/src/test/java/com/desafio/pixpay/utils/AccountFactory.java
@@ -0,0 +1,55 @@
+package com.desafio.pixpay.utils;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.desafio.pixpay.core.domain.account.Account;
+import com.desafio.pixpay.core.domain.account.FullName;
+import com.desafio.pixpay.core.domain.account.Password;
+import com.desafio.pixpay.core.domain.account.Role;
+import com.desafio.pixpay.core.domain.email.Email;
+import com.desafio.pixpay.core.domain.identification.CadastroDePessoaFisica;
+import com.desafio.pixpay.core.domain.identification.CadastroNacionalDePessoaJuridica;
+import com.desafio.pixpay.core.domain.money.Money;
+import com.desafio.pixpay.core.gateways.PasswordEncoderGateway;
+import com.desafio.pixpay.infra.security.PasswordEncoderImpl;
+
+public class AccountFactory {
+    
+    private static PasswordEncoderGateway passwordEncoderGateway = new PasswordEncoderImpl(new BCryptPasswordEncoder());
+
+    public static Account createManager(){
+        return new Account(
+            Set.of(Role.ROLE_MANAGER, Role.ROLE_USER),
+            new CadastroNacionalDePessoaJuridica().builder().fromPersistence("30764711000101"),
+            new FullName().fromPersistence("Manager Manager"),
+            new Email().fromPersistence("manager@manager.com"), 
+            new Password().fromPersistence(passwordEncoderGateway.encode("30764711000101")),
+            new Money()
+        );
+    }
+
+    public static Account createBusiness(){
+        return new Account(
+            Set.of(Role.ROLE_USER),
+            new CadastroNacionalDePessoaJuridica().builder().fromPersistence("34547820000110"),
+            new FullName().fromPersistence("Business Business"),
+            new Email().fromPersistence("business@business.com"), 
+            new Password().fromPersistence(passwordEncoderGateway.encode("34547820000110")),
+            new Money(100000L)
+        );
+    }
+
+    public static Account createPersonal(){
+        return new Account(
+            Set.of(Role.ROLE_USER),
+            new CadastroDePessoaFisica().builder().fromPersistence("94900666050"),
+            new FullName().fromPersistence("Personal Personal"),
+            new Email().fromPersistence("personal@personal.com"), 
+            new Password().fromPersistence(passwordEncoderGateway.encode("94900666050")),
+            new Money(10000L)
+        );
+    }
+}
