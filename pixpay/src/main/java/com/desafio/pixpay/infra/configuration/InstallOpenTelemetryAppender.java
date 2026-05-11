5ff87ba53fc21c297a24f2c206a7eddb9a826483
Marlon Michael <marlonmich7@gmail.com>
2026-05-11 15:33:33 -0300

    adicionado observabilidade com stack OTLP: open telemetry, tempo, loki, prometheus

---
+++ pixpay/src/main/java/com/desafio/pixpay/infra/configuration/InstallOpenTelemetryAppender.java
@@ -0,0 +1,22 @@
+package com.desafio.pixpay.infra.configuration;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+
+@Component
+public class InstallOpenTelemetryAppender implements InitializingBean {
+
+    private final OpenTelemetry openTelemetry;
+
+    InstallOpenTelemetryAppender(OpenTelemetry openTelemetry) {
+        this.openTelemetry = openTelemetry;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        OpenTelemetryAppender.install(this.openTelemetry);
+    }
+}
